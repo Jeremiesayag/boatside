96e117171c8a4a2526dcc5fc2366dd4fc1254f8d
Amir M <amcoder2@mail.com>
2018-08-13 17:07:07 +0300

    added bookings model without validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_13_134238) do
+ActiveRecord::Schema.define(version: 2018_08_13_140053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "user_id"
+    t.bigint "yacht_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+    t.index ["yacht_id"], name: "index_bookings_on_yacht_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -42,5 +53,7 @@ ActiveRecord::Schema.define(version: 2018_08_13_134238) do
     t.index ["user_id"], name: "index_yachts_on_user_id"
   end
 
+  add_foreign_key "bookings", "users"
+  add_foreign_key "bookings", "yachts"
   add_foreign_key "yachts", "users"
 end
