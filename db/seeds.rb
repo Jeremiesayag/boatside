ce5e8de96aa127771a984b1afdd6d87ad07945f3
Jeremie Sayag <jeremiesayag.priv@gmail.com>
2018-08-13 18:46:33 +0300

    added seeds with faker and column firts_name and last_name to users table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+
+User.destroy_all
+Yacht.destroy_all
+Booking.destroy_all
+
+5.times do
+  user = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: Faker::Internet.password(8))
+
+
+  2.times do
+    Yacht.create!(name: Faker::StarWars.vehicle, location: Faker::Address.city, boat_type: ["Houseboat", "Catamaran", "Sailboat", "Speedboat", "Motor boat"].sample, user: user)
+  end
+end
+
+10.times do
+start = (Date.today..3.months.from_now.to_date).to_a.sample
+Booking.create!(
+  user: User.all.sample,
+  yacht: Yacht.all.sample,
+  start_date: start,
+  end_date: start + rand(10).days
+  )
+end
+
+
+4.times do
+  User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: Faker::Internet.password(8))
+end
