63a1e1c2319233da754810b9c2d9248a3d58f1fd
Jeremie Sayag <jeremiesayag.priv@gmail.com>
2018-08-14 13:48:30 +0300

    added yachts controller and view

---
+++ app/controllers/yachts_controller.rb
@@ -0,0 +1,27 @@
+class YachtsController < ApplicationController
+  def index
+    if params[:query].present?
+      @yachts = Yacht.where("location ILIKE ?", "%#{params[:query]}%")
+    else
+      @yachts = Yacht.all
+    end
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
