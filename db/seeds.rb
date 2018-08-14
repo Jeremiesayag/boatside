42434aed7e4363a982a79d6ceed91781ff1c62a3
Jeremie Sayag <jeremiesayag.priv@gmail.com>
2018-08-14 18:18:06 +0300

    more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ User.destroy_all
 Yacht.destroy_all
 Booking.destroy_all
 
-5.times do
+7.times do
   user = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: Faker::Internet.password(8))
 
 
