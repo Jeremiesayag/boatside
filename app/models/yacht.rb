79e9bf78afd670bbbfb4577787ae0f293892d222
Jeremie Sayag <jeremiesayag.priv@gmail.com>
2018-08-16 17:01:14 +0300

    d

--- app/models/yacht.rb
+++ app/models/yacht.rb
@@ -1,6 +1,7 @@
 class Yacht < ApplicationRecord
   has_many :bookings, dependent: :destroy
   belongs_to :user
+
   validates :name, presence: true
   validates :location, presence: true
   validates :boat_type, presence: true
